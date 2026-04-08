f8327a0828ba5ba9b3f8e5720a92276ca89cffae
Marcus <marcus.paixao02@gmail.com>
2026-04-08 19:49:07 -0300

    Conlusão Desafio

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Olá Mundão");
+let Nome = "Marcus Han Solo";
+let Nivel = 9000;
+
+if (Nivel <=1000) {
+    Nivel = "Ferro"
+    console.log("o Herói de Nome", Nome, "está no nivel", Nivel)
+}
+else if (Nivel >=1001 && Nivel <=2000) {
+     Nivel = "Bronze"
+    console.log("o Herói de Nome", Nome, "está no nivel", Nivel)
+} 
+else if (Nivel >=2001 && Nivel <=5000) {
+     Nivel = "Prata"
+    console.log("o Herói de Nome", Nome, "está no nivel", Nivel)
+}
+else if (Nivel >=5001 && Nivel <=7000) {
+     Nivel = "Ouro"
+    console.log("o Herói de Nome", Nome, "está no nivel", Nivel)
+} 
+else if (Nivel >=7001 && Nivel <=8000) {
+     Nivel = "Platina"
+    console.log("o Herói de Nome", Nome, "está no nivel", Nivel)
+}
+else if (Nivel >=8001 && Nivel <=9000) {
+     Nivel = "Ascendente"
+    console.log("o Herói de Nome", Nome, "está no nivel", Nivel)
+} 
+else if (Nivel >=9001 && Nivel <=10000) {
+     Nivel = "Imortal"
+    console.log("o Herói de Nome", Nome, "está no nivel", Nivel)
+} 
+else {
+     Nivel = "Radiante"
+    console.log("o Herói de Nome", Nome, "está no nivel", Nivel)
+};
